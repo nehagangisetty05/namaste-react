bfb3be8cbd9b8f3db4a6db72dbd57ba7f33283c3
Neha <nehagangisetty@gmail.com>
2024-05-23 12:00:09 +0530

    episode-01-practise

---
+++ App.js
@@ -0,0 +1,38 @@
+/*
+const heading = React.createElement("h1", {id : "heading", xyz : "abc"}, "Hello world from react!!");
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+*/
+
+const parent = React.createElement(
+    "div", 
+    {id : "parent"}, 
+    [
+        React.createElement(
+            "div", 
+            {id: "child1"}, 
+            [
+                React.createElement("h1", {}, "Iam nested h1"),
+                React.createElement("h2", {}, "Iam nested h2")
+            ] 
+        ),
+        React.createElement(
+            "div", 
+            {id: "child2"}, 
+            [
+                React.createElement("h1", {}, "Iam nested h1"),
+                React.createElement("h2", {}, "Iam nested h2")
+            ] 
+        )
+    ]
+);
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+    
+root.render(parent);

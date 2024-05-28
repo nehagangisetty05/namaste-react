94a433b3536a299efb5be85e537cab0fceddd4c0
Neha <nehagangisetty@gmail.com>
2024-05-28 11:30:12 +0530

    added asset

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
 
-const Title = (
-    <h1>Namaste React from Title componenent</h1>
-);
-
-const HeadingComponent = () => (
-    <div id="container">
-        {Title}
-        <h1 className="heading">Namaste React🚀 - functional component</h1>
-    </div>
-);
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - RestaurantContainer
+ *      -ResraurantCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+const Header = () => {
+    return (
+        <div className="header">
+            <div>
+                <img src="../asset/foodlogo.png"></img>
+            </div>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
